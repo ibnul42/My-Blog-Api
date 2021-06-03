692b9542fcb1348ed3abc37e4e546cc4d63559d7
ibnul42 <ibnulashir42@gmail.com>
2021-06-03 21:01:09 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const Post = require("./api/modules/posts");
+const postData = new Post();
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+})
+
+app.use('/uploads', express.static('uploads'));
+
+app.get("/api/posts", (req, res) => {
+
+    res.status(200).send(postData.get());
+});
+
+app.get("/api/posts/:post_id", (req, res) => {
+    const postId = req.params.post_id;
+    const foundPost = postData.getIndividualBlog(postId);
+    if(foundPost) {
+        res.status(200).send(foundPost);
+    } else {
+        res.status(404).send("Not Found");
+    }
+    console.log(postId);
+});
+
+app.listen(3000, ()=> {
+    console.log("Listening")
+})
